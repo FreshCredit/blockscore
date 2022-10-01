f142c623368b46d5ff132b079b765917e8a3a150
NenadNedeljkovic <nedeljkovicnenadupw@gmail.com>
2022-10-01 15:41:13 +0000

    Fix some issues of Parachain

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -55,7 +55,7 @@ use xcm::latest::prelude::BodyId;
 use xcm_executor::XcmExecutor;
 
 /// Import the template pallet.
-pub use pallet_template;
+pub use pallet_scoring;
 
 /// Alias to 512-bit hash when used in the context of a transaction signature on the chain.
 pub type Signature = MultiSignature;
@@ -455,7 +455,7 @@ impl pallet_collator_selection::Config for Runtime {
 }
 
 /// Configure the pallet template in pallets/template.
-impl pallet_template::Config for Runtime {
+impl pallet_scoring::Config for Runtime {
 	type Event = Event;
 }
 
@@ -492,7 +492,7 @@ construct_runtime!(
 		DmpQueue: cumulus_pallet_dmp_queue::{Pallet, Call, Storage, Event<T>} = 33,
 
 		// Template
-		TemplatePallet: pallet_template::{Pallet, Call, Storage, Event<T>}  = 40,
+		TemplatePallet: pallet_scoring::{Pallet, Call, Storage, Event<T>}  = 40,
 	}
 );
 
